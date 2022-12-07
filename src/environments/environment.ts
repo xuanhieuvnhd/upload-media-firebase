15f336e6c36992600f52a833fb9cca325baf85a9
xuanhieuvnhd <xuanhieuvn@gmail.com>
2022-12-07 14:11:54 +0700

    7/12

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,22 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDSYG0bGHUoMrG5k-3zBVfA2voYKErVLuY",
-    authDomain: "hoanghieuhd-2022.firebaseapp.com",
-    databaseURL: 'https://hoanghieuhd-2022.firebaseio.com',
-    projectId: "hoanghieuhd-2022",
-    storageBucket: "hoanghieuhd-2022.appspot.com",
-    messagingSenderId: "10391927718",
-    appId: "1:10391927718:web:e2e53bb3cc76083832ed67",
-    measurementId: "G-PXEWCW9EVF"
+    // apiKey: "AIzaSyDSYG0bGHUoMrG5k-3zBVfA2voYKErVLuY",
+    // authDomain: "hoanghieuhd-2022.firebaseapp.com",
+    // databaseURL: 'https://hoanghieuhd-2022.firebaseio.com',
+    // projectId: "hoanghieuhd-2022",
+    // storageBucket: "hoanghieuhd-2022.appspot.com",
+    // messagingSenderId: "10391927718",
+    // appId: "1:10391927718:web:e2e53bb3cc76083832ed67",
+    // measurementId: "G-PXEWCW9EVF"
+
+    apiKey: "AIzaSyCrBOovbX9Udhcvq9mPQFDS8_KhJ5jYeWE",
+    authDomain: "htv3-8f33d.firebaseapp.com",
+    databaseURL: "https://htv3-8f33d-default-rtdb.firebaseio.com",
+    projectId: "htv3-8f33d",
+    storageBucket: "htv3-8f33d.appspot.com",
+    messagingSenderId: "30877059624",
+    appId: "1:30877059624:web:8804ba324cf539d360031d",
+    measurementId: "G-DPC29F180X"
   }
 };
